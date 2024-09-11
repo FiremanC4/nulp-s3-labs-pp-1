dc008d32cd129098985af13fc12c45a0942871a6
FiremanC4 <firemanplay66@gmail.com>
2024-09-11 23:01:25 +0300

    finished work

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 200; i++) {
-            Fibonacci fib = new Fibonacci(i);
+        Fibonacci fib;
+        for (int i = 0; i < 5; i++) {
+            fib = new Fibonacci(i);
             fib.output();
+        }
 
+        Scanner scan = new Scanner(System.in);
+        int n;
+        System.out.print("\nEnter number of fibonacci numbers, -1 to exit\n");
+        while (true) {
+            n = scan.nextInt();
+            if (n == -1) break;
+
+            fib = new Fibonacci(n);
+            fib.output();
         }
     }
 }
